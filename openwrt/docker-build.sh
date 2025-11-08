a01d4e727a3d241c801b6be8e83f9e522bcef03d
adjlevis <adjlevis@qq.com>
2025-11-08 21:00:46 +0800

    Update docker-build.sh

--- openwrt/docker-build.sh
+++ openwrt/docker-build.sh
@@ -8,8 +8,9 @@ docker run --rm \
   -v "$WORKDIR/bin:/home/build/immortalwrt/bin" \
   -v "$WORKDIR/files:/home/build/immortalwrt/files" \
   -v "$WORKDIR/build.sh:/home/build/immortalwrt/build.sh" \
-openwrt/imagebuilder:layerscape-armv8_64b-master \
+openwrt/imagebuilder:armsr-armv8 \
   /home/build/immortalwrt/build.sh
+
 
 
 
