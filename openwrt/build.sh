a5a18e2ea709cf594b50c3a785c05e1ab0adaa4a
adjlevis <adjlevis@qq.com>
2025-11-09 12:03:23 +0800

    Update build.sh

--- openwrt/build.sh
+++ openwrt/build.sh
@@ -17,4 +17,5 @@ PACKAGES="$PACKAGES luci-i18n-base-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-package-manager-zh-cn"
 
-make image PACKAGES="$PACKAGES" ROOTFS_PARTSIZE="512"
+# ✅ 指定构建目标（比如 generic 或 onecloud）
+make image PROFILE="N1" PACKAGES="$PACKAGES" ROOTFS_PARTSIZE="512"
