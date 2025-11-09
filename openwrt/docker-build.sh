8ed0cc999bdb40f0ebaa08387e4b47063fa66d18
adjlevis <adjlevis@qq.com>
2025-11-09 12:16:09 +0800

    Update docker-build.sh

--- openwrt/docker-build.sh
+++ openwrt/docker-build.sh
@@ -6,8 +6,8 @@ WORKDIR="$SCRIPT_DIR"
 
 docker run --rm \
   --user root \
-  -v "$WORKDIR/bin:/home/build/openwrt/bin" \
-  -v "$WORKDIR/files:/home/build/openwrt/files" \
-  -v "$WORKDIR/build.sh:/home/build/openwrt/build.sh" \
-  openwrt/imagebuilder:armsr-armv8 \
-  /home/build/openwrt/build.sh
+  -v "$WORKDIR/bin:/builder/bin" \
+  -v "$WORKDIR/files:/builder/files" \
+  -v "$WORKDIR/build.sh:/builder/build.sh" \
+  immortalwrt/imagebuilder:armsr-armv8 \
+  bash -c "cd /builder && ./build.sh"
