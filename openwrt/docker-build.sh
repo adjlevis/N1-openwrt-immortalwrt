501a7dfbdba323a1902681e987d58f5199160b47
adjlevis <adjlevis@qq.com>
2025-11-09 12:05:56 +0800

    Update docker-build.sh

--- openwrt/docker-build.sh
+++ openwrt/docker-build.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 set -euxo pipefail
 
-WORKDIR=$(pwd)
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
+WORKDIR="$SCRIPT_DIR"
 
 docker run --rm \
   --user root \
   -v "$WORKDIR/bin:/home/build/openwrt/bin" \
   -v "$WORKDIR/files:/home/build/openwrt/files" \
   -v "$WORKDIR/build.sh:/home/build/openwrt/build.sh" \
-openwrt/imagebuilder:armsr-armv8 \
+  openwrt/imagebuilder:armsr-armv8 \
   /home/build/openwrt/build.sh
-
-
-
-
-
-
-
